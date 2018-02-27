0a4007af48e10943134c27d54c8a7d1ff0971860
davidkaggulire <dkaggulire@gmail.com>
2018-02-27 23:13:18 +0300

    Adds update, delete methods to models

--- app/models.py
+++ app/models.py
@@ -1,55 +1,52 @@
 #models.py
-
+from flask import jsonify
 
 class User(object):
-    def __init__(self, id, username, password, message, status):
-        self.id = id
-        self.username = username
-        self.password = password
-        self.message = message
-        self.status = status
+    def __init__(self):
+        self.user_list = []
 
-    def password_reset(self, password, ):
-        self.password = password
-        return "password has been reset"
+    def register(self, id, firstname, lastname, username, password, email, gender):
+        """ registers user if user doesnot exist """
+        user_details = {}
 
-    def __str__(self):
-        return "User(id='%s')" % self.id
+        for user in self.user_list:
+            if user['username'] == username or user['email'] == email:        
+                return "Username or email exists"
+        else:
+            user_details['id'] = id
+            user_details['firstname'] = firstname
+            user_details['lastname'] = lastname
+            user_details['username'] = username
+            user_details['password'] = password
+            user_details['email'] = email
+            user_details['gender'] = gender
+            
+            self.user_list.append(user_details)        
+            return "User successfully added"
 
-class Business(object):
-    def __init__(self, id, businessname, location, category, address, email, phonenumber):
-        self.id = id
-        self.businessname = businessname
-        self.location = location
-        self.category = category
-        self.address = address
-        self.email = email
-        self.phonenumber = phonenumber
+    def login(self, username, password):
+        """ login user given valid credentials """
+        for user in self.user_list:
+            if user['username'] == username:
+                if user['password'] == password:
+                    return "User successfully logged in"
+                else:
+                    return "Incorrect credentials"
+        return "User doesnot exist"
 
-    def __str__(self):
-        return "Business(id='%s')"%self.id
+    def password_reset(self, username, newpassword):
+        for user in self.user_list:
+            if user['username'] == username:
+                user['password'] = newpassword
+                return "Password reset successfully"
+        else:
+            return "Failed to reset"
 
-    def update_registered_business(self, id, businessname, location, category, address, email, phonenumber):
-        pass        
 
-class Signup(object):
-    def __init__(self, id, firstname, lastname, username, password, email, phonenumber, gender):
-        self.id = id
-        self.firstname = firstname
-        self.lastname = lastname
-        self.username = username
-        self.password = password 
-        self.email = email,
-        self.phonenumber = phonenumber
-        self.gender = gender
+# class Reviews(object):
+#     def __init__(self, id, review):
+#         self.id = id
+#         self.review = review
 
-    def __str__(self):
-        return "Signup(id = '%s')" % self.id
-
-class Reviews(object):
-    def __init__(self, id, review):
-        self.id = id
-        self.review = review
-
-    def __str_(self):
-        return "Review (id = '%s') " % self.id
+#     def __str_(self):
+#         return "Review (id = '%s') " % self.id
