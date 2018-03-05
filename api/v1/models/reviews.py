d93eddd6168fb7ab5f7d7af2f03299b81db31947
davidkaggulire <dkaggulire@gmail.com>
2018-03-06 00:15:34 +0300

    Edits reviews.py

--- api/v1/models/reviews.py
+++ api/v1/models/reviews.py
@@ -1,24 +1,26 @@
 """ reviews.py """
 
 from flask import jsonify
-reviews_list = []
+
 class Reviews():
+    def __init__(self):
+        self.reviews_list = []
     def post_review(self, business_id, new_review):
         """ post review about business """
         reviews = {}
-        for review in reviews_list:
+        for review in self.reviews_list:
             if review['business_id'] == business_id:
                 return "Business already reviewed"
         else:
             reviews['business_id'] = business_id
             reviews['new_review'] = new_review
-            reviews_list.append(reviews)
+            self.reviews_list.append(reviews)
             return "Successfully added review"
-
+shakira.seruwagi@andela.com
     def get_reviews(self, business_id):
         """ get reviews about business """
-        for review in reviews_list:
+        for review in self.reviews_list:
             if review['business_id'] == business_id:
                 return review
         
-        return reviews_list
+        return self.reviews_list
